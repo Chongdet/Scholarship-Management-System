e9efb3dbe884a9139beda4707d3ea5715de6f7fb
Chongdet <chongdet.ja.68@ubu.ac.th>
2026-02-12 16:57:01 +0700

    ddsfffffsdd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask insisssssssssssde Docker!"
+    return "Hello from Flask insisssssasdsssssssde Docker!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
